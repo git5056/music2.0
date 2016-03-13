e8d95a769eb160d56acc8ef3aeb1ae24b1acf59c
git5056 <user5056@163.com>
2016-03-13 18:52:26 +0800

    new

---
+++ Manager/impl/TestImpl.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Manager.impl
+{
+    class TestImpl:inte.ITest
+    {
+        public void nop()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
